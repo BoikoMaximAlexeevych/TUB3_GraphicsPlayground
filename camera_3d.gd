ebb9d97d2ddc8fd29ed01d492ff18d916b01fcda
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2026-03-12 09:38:03 +0300

    delta movement

--- camera_3d.gd
+++ camera_3d.gd
@@ -2,8 +2,7 @@ extends Camera3D
 
 func _process(delta):
 	var input = Vector2(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")), 
-	int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))) * .05
+	int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))) * delta * 10
 	
-	var vertical_input = (int(Input.is_action_pressed("Ascend")) - int(Input.is_action_pressed("Descend")))*0.02
-	
+	var vertical_input = (int(Input.is_action_pressed("Ascend")) - int(Input.is_action_pressed("Descend")))* delta * 5	
 	self.position += Vector3(input.x, vertical_input, input.y)
