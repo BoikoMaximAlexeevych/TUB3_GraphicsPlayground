31b61bc9d3ebb860cecaabeb4a6814315e8cd49b
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2026-03-12 10:21:59 +0300

    tweaks+cam movement

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,8 +1,20 @@
 extends Camera3D
 
+var previous_input: Vector2
+var dif: float
+
 func _process(delta):
 	var input = Vector2(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")), 
 	int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))) * delta * 10
 	
 	var vertical_input = (int(Input.is_action_pressed("Ascend")) - int(Input.is_action_pressed("Descend")))* delta * 5	
+	
+	if input != previous_input:
+		dif = sign(input.angle_to(previous_input)) * (input - previous_input).length()
+		print("input changed: ", dif)
+		previous_input = input
+
+	self.rotation.y = lerp(self.rotation.y, dif, 0.1)
+	
 	self.position += Vector3(input.x, vertical_input, input.y)
+	
