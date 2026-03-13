0fbc0f8e37ff41828df8ec30c7bee7ac527856b0
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2026-03-13 23:11:08 +0300

    new movement
    probably fits better to our characted movement

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -1,20 +1,34 @@
 extends Camera3D
 
-var previous_input: Vector2
-var dif: float
+const ACCELERATION: float = 30.
+const MAX_SPEED: float = 80.
+
+var angular_speed: float
+var rot_angle: float
+var move_speed: float
 
 func _process(delta):
-	var input = Vector2(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")), 
-	int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))) * delta * 10
 	
 	var vertical_input = (int(Input.is_action_pressed("Ascend")) - int(Input.is_action_pressed("Descend")))* delta * 5	
 	
-	if input != previous_input:
-		dif = sign(input.angle_to(previous_input)) * (input - previous_input).length()
-		print("input changed: ", dif)
-		previous_input = input
-
-	self.rotation.y = lerp(self.rotation.y, dif, 0.1)
+	var angular_input = int(Input.is_action_pressed("ui_left")) - int(Input.is_action_pressed("ui_right"))
+	angular_speed += angular_input * delta * 5
+	rot_angle += angular_speed * delta
+	if(not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right")):
+		angular_speed = move_toward(angular_speed, 0., 0.1)
 	
-	self.position += Vector3(input.x, vertical_input, input.y)
+	var move_input = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
+	move_speed -= ACCELERATION * delta * move_input
+	move_speed = clamp(move_speed, -MAX_SPEED, MAX_SPEED)
+	var direction = Vector2(0., 1.).rotated(-rot_angle) * move_speed * delta
+	
+	
+	self.position.x += direction.x
+	self.position.z += direction.y
+	
+	if(move_input == 0):
+		move_speed = move_toward(move_speed, 0., 0.3)
+	
+	self.rotation.y = rot_angle
+	
 	
